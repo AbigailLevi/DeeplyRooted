11c7096e9480629d3a3a10b56b89703172190de9
NCONTED <NCONTED@mynorth.mymdc.net>
2020-03-04 15:22:35 -0500

    fixing logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import image from "../assets/Deeply.png";
 
-
 const NavBar = () => {
   return (
     <nav className="navbar navbar-light">
       <a className="navbar-brand" href="#">
-        <img src={image} width="150" height="75" alt="" />
+        <img className="logo-img img-fluid" src={image} alt="logo" />
       </a>
     </nav>
   );
